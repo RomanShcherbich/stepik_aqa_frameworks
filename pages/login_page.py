2062673b76ad34d5a9358dd048cd75b4f7fcc720
Roman <rshcherbich@lyte.com>
2023-01-02 10:57:35 +0800

    implement login page

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from locators import LoginPageLocators
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, f"invalid url [{self.browser.current_url}] in login page"
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), f"Login form element not found"
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM), f"Registration form element not found"
