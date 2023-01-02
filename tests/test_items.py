80a2ce5142a03add517d7377d870f5974e641f75
Roman <rshcherbich@lyte.com>
2023-01-02 08:15:38 +0800

    reformatting test code

--- tests/test_items.py
+++ tests/test_items.py
@@ -8,7 +8,8 @@ LANGUAGE_MAP = {
 }
 
 
-def test_guest_should_see_login_link(browser, browser_language):
+def test_add_to_basket_button(browser, browser_language):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    basket_button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button")
-    assert basket_button.text == LANGUAGE_MAP.get(browser_language)
+    basket_button_list = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")
+    assert basket_button_list, "the button cannot be found by the locator"
+    assert basket_button_list[0].text == LANGUAGE_MAP.get(browser_language)
